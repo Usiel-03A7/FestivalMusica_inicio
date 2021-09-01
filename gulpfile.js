f080954a47e6023b435607cbc53cc2ee0d81336f
Usiel-03A7 <shebastyanmail.com>
2021-09-01 11:25:13 -0500

    learn to install gulp images and import

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const {series, src, dest, watch} = require('gulp'); // para importar
 const sass = require('gulp-sass')(require('sass'));
+// const imagemin = import('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
 
 
 function css( ) {
-    return src('src/scss/app.scss')
+    return src('./src/scss/app.scss')
     .pipe(sass({
         outputStyle: 'expanded'
     }))
@@ -16,9 +18,15 @@ function mini( ) {
     }))
     .pipe(dest('./build/css'))
 }
+function imagenes() {
+    return src('./src/img/**/*')
+    .pipe( imagemin() )
+    .pipe(dest('./build/img'))
+    
+}
 function watchArch() {
     watch('./src/scss/**/*.scss', css); // * sirve para archivos igual y del mismo nivel... ** todos los archivos debajo del nivel con esa extención
 }
-
+exports.imagenes = imagenes;
 exports.watchArch = watchArch;
 exports.mini = mini;
